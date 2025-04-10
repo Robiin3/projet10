e5338907ff9625ee21e7bb601c0f5e492ad86738
Robiin <robin.molina@hotmail.fr>
2025-04-10 14:48:51 +0200

    Simplification test XSS annulée

--- cypress/e2e/XSS_Reviews.cy.js
+++ cypress/e2e/XSS_Reviews.cy.js
@@ -1,6 +1,5 @@
 describe('XSS Tests - Reviews', () => {
   const xssPayload = '<script>alert("XSS")</script>';
-  let authToken; // Stocke le token pour réutilisation
 
   describe('Frontend XSS Tests', () => {
     beforeEach(() => {
@@ -9,10 +8,7 @@ describe('XSS Tests - Reviews', () => {
       cy.get('[data-cy="nav-link-login"]').click();
       cy.get('[data-cy="login-input-username"]').type(Cypress.env('loginUsername'));
       cy.get('[data-cy="login-input-password"]').type(Cypress.env('loginPassword'));
-      cy.get('[data-cy="login-submit"]').click().then(() => {
-        // Récupère le token depuis le localStorage après connexion
-        authToken = localStorage.getItem('user');
-      });
+      cy.get('[data-cy="login-submit"]').click();
       cy.get('[data-cy="nav-link-cart"]').should("be.visible");
       cy.get('[data-cy="nav-link-reviews"]').should('be.visible').click(); // Accède à la page des avis après connexion
     });
@@ -31,13 +27,30 @@ describe('XSS Tests - Reviews', () => {
   });
 
   describe('Backend XSS Tests', () => {
+    let authToken;
+
+    before(() => {
+      // Connexion API
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env('apiUrl')}/login`,
+        body: {
+          username: Cypress.env('loginUsername'),
+          password: Cypress.env('loginPassword')
+        }
+      }).then((response) => {
+        expect(response.status).to.equal(200); // Vérifie que la connexion est réussie
+        authToken = response.body.token; // Stocke le token pour les tests suivants
+      });
+    });
+
     it('test XSS vulnerability in review submission', () => {
       // Envoie un avis contenant un script XSS via l'API
       cy.request({
         method: 'POST',
         url: `${Cypress.env('apiUrl')}/reviews`,
         headers: {
-          Authorization: `Bearer ${authToken}` // Réutilise le token récupéré lors de la connexion frontend
+          Authorization: `Bearer ${authToken}` // Utilise le token pour l'authentification
         },
         body: {
           title: "XSS Test", // Titre de l'avis
